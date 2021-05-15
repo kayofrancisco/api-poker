72ea8e4f60488acfcb444abb5610cc48e8f7db89
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2021-05-14 23:46:22 -0300

    Iniciado documentação com swagger

--- src/routes/intex.ts
+++ src/routes/intex.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
-import clubesRoutes from './clubes.routes';
 import modalidadesRoutes from './modalides.routes';
 
 const router = Router();
 
-router.use('/clubes', clubesRoutes);
 router.use('/modalidades', modalidadesRoutes);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 
 import rotas from './routes/intex';
+import arquivoSwagger from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swagger.serve, swagger.setup(arquivoSwagger));
 app.use(rotas);
 
 app.listen(3333, () => console.log('server is running'));
